db5c1ecba6a7329221324bb18e8717c2d51ca13c
Gregory Bowers <bowers.58@wright.edu>
2019-05-04 12:23:52 -0400

    Implemented rudimentary method to generate up to 11 distinguishable colors.

--- Capstone Application/ImageTrace.cs
+++ Capstone Application/ImageTrace.cs
@@ -15,6 +15,45 @@ namespace Capstone_Application
         ControllerScript controllerScript = Form1.controllerScript;
         int startX = 4;
         int startY = 7;
+        bool generateColors = true;
+        //https://stackoverflow.com/questions/470690/how-to-automatically-generate-n-distinct-colors
+        List<Color> colors = new List<Color>
+        {
+            UIntToColor(0xFFFFB300), //Vivid Yellow
+            UIntToColor(0xFF803E75), //Strong Purple
+            UIntToColor(0xFFFF6800), //Vivid Orange
+            UIntToColor(0xFFA6BDD7), //Very Light Blue
+            UIntToColor(0xFFC10020), //Vivid Red
+            UIntToColor(0xFFCEA262), //Grayish Yellow
+            UIntToColor(0xFF817066), //Medium Gray
+
+            //The following will not be good for people with defective color vision
+            UIntToColor(0xFF007D34), //Vivid Green
+            UIntToColor(0xFFF6768E), //Strong Purplish Pink
+            UIntToColor(0xFF00538A), //Strong Blue
+            UIntToColor(0xFFFF7A5C), //Strong Yellowish Pink
+            UIntToColor(0xFF53377A), //Strong Violet
+            UIntToColor(0xFFFF8E00), //Vivid Orange Yellow
+            UIntToColor(0xFFB32851), //Strong Purplish Red
+            UIntToColor(0xFFF4C800), //Vivid Greenish Yellow
+            UIntToColor(0xFF7F180D), //Strong Reddish Brown
+            UIntToColor(0xFF93AA00), //Vivid Yellowish Green
+            UIntToColor(0xFF593315), //Deep Yellowish Brown
+            UIntToColor(0xFFF13A13), //Vivid Reddish Orange
+            UIntToColor(0xFF232C16), //Dark Olive Green
+        };
+        private static readonly List<Color> _boyntonOptimized = new List<Color>
+        {
+            Color.FromArgb(0, 0, 255),      //Blue
+            Color.FromArgb(255, 0, 0),      //Red
+            Color.FromArgb(0, 255, 0),      //Green
+            Color.FromArgb(255, 255, 0),    //Yellow
+            Color.FromArgb(255, 0, 255),    //Magenta
+            Color.FromArgb(255, 128, 128),  //Pink
+            Color.FromArgb(128, 128, 128),  //Gray
+            Color.FromArgb(128, 0, 0),      //Brown
+            Color.FromArgb(255, 128, 0),    //Orange
+        };
         List<CheckBox> checklist = new List<CheckBox>();
         DirectBitmap bmp;
         TraceOption action = TraceOption.Heatmap;
@@ -173,12 +212,11 @@ namespace Capstone_Application
                 List<Tuple<int, int, int>> history = controllerScript.Paths[agent_locations[i]];
                 List<Tuple<int, int, int>> unique_history = history.Distinct().ToList();
                 List<Tuple<int, int>> unique_history_locations = unique_history.Select(x => new Tuple<int, int>(x.Item1, x.Item2)).ToList();
-
                 for (int j = 0; j < unique_history.Count; j++)
                 {
                     Tuple<int, int> cur_loc = new Tuple<int, int>(unique_history[j].Item1, unique_history[j].Item2);
                     double alpha = Convert.ToDouble(j + 1) / unique_history.Count();
-                    Color cur_color = controllerScript.colors[unique_history[j].Item3];
+                    Color cur_color = generateColors? _boyntonOptimized[i] : controllerScript.colors[unique_history[j].Item3];
                     Color new_color = Color.FromArgb((int)(cur_color.R * alpha), (int)(cur_color.G * alpha), (int)(cur_color.B * alpha));
                     if (already_colored.Contains(cur_loc))
                     {
@@ -277,7 +315,7 @@ namespace Capstone_Application
             sfd.Filter = "(*.bmp)|*.bmp|(*.jpeg)|*.jpeg|(*.png)|*.png|(*.tiff)|*.tiff";
             sfd.FileName = DateTime.Now.Year.ToString() + "-" + DateTime.Now.Month.ToString() + "-" + DateTime.Now.Day.ToString() + " " +
                 DateTime.Now.Hour.ToString() + "-" + DateTime.Now.Minute.ToString() + "-" + DateTime.Now.Second.ToString() + " Run " +
-                controllerScript.caRuns + " Heatmap/Trace.bmp";
+                controllerScript.caRuns + " Heatmap-Trace.bmp";
             if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
                 tracePictureBox.Image.Save(sfd.FileName);
@@ -296,6 +334,15 @@ namespace Capstone_Application
         private void backgroundWorker1_ProgressChanged_1(object sender, ProgressChangedEventArgs e)
         {
             this.progressBar1.Value = e.ProgressPercentage;
+        }
+
+        static public Color UIntToColor(uint color)
+        {
+            var a = (byte)(color >> 24);
+            var r = (byte)(color >> 16);
+            var g = (byte)(color >> 8);
+            var b = (byte)(color >> 0);
+            return Color.FromArgb(a, r, g, b);
         }
 
         enum TraceOption
