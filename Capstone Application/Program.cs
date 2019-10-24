ff80b52e48644ff6c1c49ce9fff5ca6876ff3a12
Gregory Bowers <bowers.58@wright.edu>
2019-10-24 03:34:10 -0400

    App center enabled

--- Capstone Application/Program.cs
+++ Capstone Application/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace Capstone_Application
 {
@@ -14,6 +17,9 @@ namespace Capstone_Application
         [STAThread]
         static void Main()
         {
+            AppCenter.LogLevel = LogLevel.Verbose;
+            AppCenter.Start("9a8f87b3-b2a6-443b-a68e-16dcb80c7c46",
+                   typeof(Analytics), typeof(Crashes));
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
